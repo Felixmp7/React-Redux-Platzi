112b6c1d4fa3efd0ea73459f948a1e8a3860d55e
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-18 19:56:06 -0400

    Creando primer componente

---
+++ platzi-video/src/playlist/components/media.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Media extends React.Component {
+  render(){
+    return(
+      <div>
+        <p>Hola Mundo</p>
+      </div>
+    )
+  }
+}
+
+export default Media;
