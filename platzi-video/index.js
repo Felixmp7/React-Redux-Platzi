23f60f79826a2712407b706f6d07ea19f0fe390e
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-18 15:10:50 -0400

    Renderizando h1 como prueba

--- platzi-video/index.js
+++ platzi-video/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom'
 
 const app = document.getElementById('app')
 // ReactDOM.render(que voy a renderizar, en donde?)
-ReactDOM.render(, app)
+ReactDOM.render(<h1>Hola Mundo</h1>, app)
