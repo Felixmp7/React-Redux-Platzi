324bed4c341b72abb32aeb3b802d0daf32c63055
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-19 11:48:21 -0400

    Añadiendo teoría sobre el ciclo de vida de los componentes

--- platzi-video/src/playlist/components/Media.js
+++ platzi-video/src/playlist/components/Media.js
@@ -56,3 +56,56 @@ Media.propTypes = {
 }
 
 export default Media;
+
+/*
+class MiComponente extends Components{
+
+	--------------------------FASE DE MONTADO--------------------------------
+	constructor(){
+	Me sirve para bindear elementos o inicializar estados
+	}
+	componentWillMount(){
+    Se ejecuta antes de montar el componente
+    Se podría usar para hacer un setState()
+	}
+	render(){
+    Contiene todos los elementos a renderizar
+    podrías usarlo para calcular propiedades (ej: concatenar una cadena)
+	}
+	componentDidMount(){
+    Metodo que se llama cuando el componente ya se renderizó,
+  	Solo se lanza una vez,
+  	Ideal para llamar a una API, hacer un setInteval, etc
+	}
+
+	--------------------------FASE DE ACTUALIZACION--------------------------------
+	componentWillReceiveProps(){
+	 Es llamado cuando el componente recibe nuevas propiedades.
+
+	}
+	shouldComponentUpdate(){
+  	Idea para poner una condición y  si las propiedades que le llegaron anteriormente
+  	 eran las mismas que tenia, retornar false para evitar re-renderear el componente
+	}
+	componentWillUpdate(){
+	 metodo llamado antes de re-renderizar el componente si shouldComponentUpdate devolvió true
+	}
+
+	 re-render si es necesario...
+
+	componentDidUpdate(){
+	 Método llamado luego del re-render, cuando el componente ya se actualizó
+	}
+
+  -------------------------FASE DE SALIDA (DESMONTADO)-------------------------------
+	componentWillUnmount(){
+	 Método llamado antes de desmontar el componente
+	}
+
+  --------------------------------MANEJO DE ERRORES-------------------------------
+	componentDidCatch(){
+	   Si ocurre algún error, lo capturo desde acá:
+	}
+
+}
+*/
