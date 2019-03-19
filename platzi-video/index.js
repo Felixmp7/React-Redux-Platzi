858b819d6459114596a235e1fd317b8eb78fc353
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-18 20:05:40 -0400

    Limpiando el codigo

--- platzi-video/index.js
+++ platzi-video/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {render} from 'react-dom'
 import Media from './src/playlist/components/Media'
 
 const app = document.getElementById('app')
 // const holaMundo = <h1>Hola Mundo</h1>
 // ReactDOM.render(que voy a renderizar, en donde?)
-ReactDOM.render(<Media/>, app)
+render(<Media/>, app)
--- platzi-video/src/playlist/components/Media.js
+++ platzi-video/src/playlist/components/Media.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-class Media extends React.Component {
+class Media extends Component {
   render(){
     return(
       <div>
