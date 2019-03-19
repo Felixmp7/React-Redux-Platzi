aa9d70bb8c53c8306dda9f73f58159d692a00ae8
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-18 21:09:03 -0400

    Otra forma de utilizar props

--- platzi-video/src/playlist/components/Media.js
+++ platzi-video/src/playlist/components/Media.js
@@ -4,26 +4,24 @@ import bitCoin from '../images/covers/bitcoin.jpg'
 
 class Media extends Component {
   render(){
-    const styles = {
-      container: {
-        color: '#44546b',
-        cursor: 'pointer',
-        width: 260,
-        border: '1px solid red',
-      }
-    }
+    const { author, image, title} = this.props
+    //Esto es una forma de utilizar constantes para ahorrar la sintaxis this.props
     return(
       <div className="Media">
         <div className="Media-cover">
           <img
             className="Media-image"
-            src={this.props.image}
+            // src={this.props.image}
+            src={image}
             alt="bitcoin"
             width={260}
             height={160}
           />
-          <h3 className="Media-title">{this.props.title}</h3>
-          <p className="Media-author">{this.props.author}</p>
+          {/* <h3 className="Media-title">{this.props.title}</h3> */}
+          <h3 className="Media-title">{title}</h3>
+          {/* <p className="Media-author">{this.props.author}</p> */}
+          <p className="Media-author">{author}</p>
+
         </div>
       </div>
     )
