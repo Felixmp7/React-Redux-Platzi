5cb1f43a6704ec4c285a44f47dd01b3bc4e78344
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-19 20:15:58 -0400

    Cumpliendo reto de Leonidas, renderizando todas las categorias con los distintos playlist

--- platzi-video/src/playlist/components/Playlist.js
+++ platzi-video/src/playlist/components/Playlist.js
@@ -5,12 +5,21 @@ import './playlist.css'
 class Playlist extends Component {
   render(){
     // console.log(this.props.data)
-    const playlist = this.props.data.categories[0].playlist
+    // const playlist = this.props.data.categories[0].playlist
+    const categories = this.props.data.categories
     return(
       <div className="Playlist">
         {
-          playlist.map( item => {
-            return <Media {...item} key={item.id}/>
+          categories.map( categorie =>{
+            return (
+              <div className="Categorie" key={categorie.id}>
+                {
+                  categorie.playlist.map( item => {
+                    return <Media {...item} key={item.id}/>
+                  })
+                }
+              </div>
+            )
           })
         }
       </div>
@@ -19,3 +28,5 @@ class Playlist extends Component {
 }
 
 export default Playlist;
+
+// {/* return <Media {...item} key={item.id}/> */}
