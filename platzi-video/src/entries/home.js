643c378971777d66fe43d1161c3fb47d2cdf1734
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-20 11:42:18 -0400

    Reorganizando carpetas para utilizar dumb components y smart components

--- platzi-video/src/entries/home.js
+++ platzi-video/src/entries/home.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import {render} from 'react-dom'
-import data from './src/playlist/api.json'
-import Playlist from './src/playlist/components/Playlist'
+import Home from '../pages/containers/home'
+// import data from './src/playlist/api.json'
+// import Playlist from './src/playlist/components/Playlist'
 
 const app = document.getElementById('app')
 // const holaMundo = <h1>Hola Mundo</h1>
 // ReactDOM.render(que voy a renderizar, en donde?)
-render(<Playlist data={data}/>, app)
+render(<Home/>, app)
---
+++ platzi-video/src/pages/components/home-layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomeLayout = props => {
+  return(
+    <section>
+      layout
+      {props.children}
+    </section>
+  )
+}
+
+export default HomeLayout
