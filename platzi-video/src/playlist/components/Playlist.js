2bb29b84db8d933767a2ddcc11018899f9b477c4
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-19 21:02:24 -0400

    Añadiendo teoria de Smarts y Dumbs components

--- platzi-video/src/playlist/components/Playlist.js
+++ platzi-video/src/playlist/components/Playlist.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Media from './Media'
 import './playlist.css'
 
-const Playlist = (props) => {
+const Playlist = props => {
   const categories = props.data.categories
   return (
         <div className="Playlist">
@@ -25,4 +25,26 @@ const Playlist = (props) => {
 
 export default Playlist;
 
-// {/* return <Media {...item} key={item.id}/> */}
+// Dumb Components
+// Cómo se ve?
+//
+// Puede contener smart components u otros componentes de UI
+// Permiten composición con `[props.children]``
+// No depeden del resto de la aplicación
+// No especifica cómo los datos son cargados o mutados
+// Recibe datos y callbacks solo con propiedades
+// Rara vez tienen su propio estado
+// Están escritos como componentes funcionales a menos que necesiten mejoras de performance.
+// Sólo pueden ser Componentes funcionales o Pure Components
+
+// Smart Components
+// Qué hace?
+
+// Concetrado en el funcionamiento de la aplicación
+// Contienen componentes de UI u otros containers
+// No tienen estilos
+// Proveen de datos a componentes de UI u otros contenedores
+// Proveen de callbacks a la UI
+// Normalmente tienen estado
+// Llaman acciones
+// Generados por higher order components
