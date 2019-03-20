6bbbb6d3ed76cf414f7ec1acbdcf95949b3f12aa
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-20 13:10:30 -0400

    Creando componente de logo

---
+++ platzi-video/src/pages/components/Related.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logoPlatziVideo from '../../logo.png'
+
+const Related = props => {
+  return(
+    <div>
+      <img src={logoPlatziVideo} alt="logo-platzi-video" width={250}/>
+    </div>
+  )
+}
+
+export default Related
