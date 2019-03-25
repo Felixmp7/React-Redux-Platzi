656f4320508e012e9f8cbe74d03200a251ca64ad
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-25 09:40:32 -0400

    Creando funcion onChange y pasandole valor por defecto al buscador desde el state

--- platzi-video/src/widgets/components/Search.js
+++ platzi-video/src/widgets/components/Search.js
@@ -7,11 +7,24 @@ const Search = props => (
     onSubmit={props.handleSubmit}
   >
     <input
+      ref={props.setRef}
       type="text"
       className="Search-input"
       name="search"
+      value={props.value}
+      onChange={props.handleChange}
     />
   </form>
 )
 
 export default Search
+
+/*
+Referencias, son la mejor forma de hacer referencia a elementos html dentro
+de React
+
+Las referencias nos permiten almacenar en react un elemento HTML
+
+value => Nos permite editar el input a menos que se envie el valor mediante props.
+defaultValue => permite editar el input
+*/
--- platzi-video/src/widgets/containers/SearchContainer.js
+++ platzi-video/src/widgets/containers/SearchContainer.js
@@ -2,14 +2,30 @@ import React, {Component} from 'react'
 import Search from '../components/Search'
 
 class SearchContainer extends Component {
+  state = {
+    value: 'Luis Fonsi'
+  }
   handleSubmit = event => {
     event.preventDefault()
-    console.log('Submit')
+    console.log(this.input.value ,'Submit')
+  }
+
+  setInputRef = element => {
+    this.input = element
+  }
+
+  handleInputChange = event => {
+    this.setState({
+      value: event.target.value
+    })
   }
   render() {
     return(
       <Search
         handleSubmit={this.handleSubmit}
+        setRef={this.setInputRef}
+        handleChange={this.handleInputChange}
+        value={this.state.value}
       />
     )
   }
