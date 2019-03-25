5efed38aeafe0a371d0a319550ab8e5ee6d536df
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-25 08:57:51 -0400

    Enviando funcion por props

--- platzi-video/src/widgets/containers/SearchContainer.js
+++ platzi-video/src/widgets/containers/SearchContainer.js
@@ -4,6 +4,7 @@ import Search from '../components/Search'
 class SearchContainer extends Component {
   handleSubmit = event => {
     event.preventDefault()
+    console.log('Submit')
   }
   render() {
     return(
