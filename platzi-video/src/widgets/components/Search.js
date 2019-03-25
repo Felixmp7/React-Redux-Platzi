e31828df0b54c60ad654080d330965e5f34bdafb
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-25 08:54:59 -0400

    Adquiriendo funcion handleSubmit por props

--- platzi-video/src/widgets/components/Search.js
+++ platzi-video/src/widgets/components/Search.js
@@ -2,10 +2,14 @@ import React from 'react'
 import './Search.css'
 
 const Search = props => (
-  <form className="Search">
+  <form
+    className="Search"
+    onSubmit={props.handleSubmit}
+  >
     <input
       type="text"
       className="Search-input"
+      name="search"
     />
   </form>
 )
