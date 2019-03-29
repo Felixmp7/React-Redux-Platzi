8626439e511060990eb6a000e70208e8e1956fef
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-29 10:20:56 -0400

    Creando componente de titulo y enlazando estilos css

---
+++ platzi-video/src/player/components/Title.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Title.css'
+
+export default const Title = props => (
+  <div className="Title">
+    <h2>{props.title}</h2>
+  </div>
+)
--- platzi-video/src/player/containers/video-player.js
+++ platzi-video/src/player/containers/video-player.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import VideoPlayerLayout from '../components/VideoPlayerLayout'
 import Video from '../components/Video'
+import Title from '../components/Title'
 
 class VideoPlayerContainer extends Component {
   render() {
     return(
       <VideoPlayerLayout>
+        <Title/>
         <Video
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
           autoplay={true}
