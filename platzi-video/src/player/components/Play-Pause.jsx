fb255c5d6fc771cdaa4a8f4d425ee1604e3c351f
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-04 00:10:09 -0400

    Utilizando if ternario para mostrar botones dependiendo del state

--- platzi-video/src/player/components/Play-Pause.jsx
+++ platzi-video/src/player/components/Play-Pause.jsx
@@ -5,16 +5,21 @@ import './Play-Pause.css'
 
 const PlayPause = props => (
   <div className="PlayPause">
-    <button
-      onClick={props.handleClick}
-    >
-      <Play size={25} color="white"/>
-    </button>
-    <button
-      onClick={props.handleClick}
-    >
-      <Pause size={25} color="white"/>
-    </button>
+    {
+      props.pause ?
+        <button
+          onClick={props.handleClick}
+        >
+          <Play size={25} color="white"/>
+        </button>
+
+      :
+        <button
+          onClick={props.handleClick}
+        >
+          <Pause size={25} color="white"/>
+        </button>
+    }
   </div>
 )
 
