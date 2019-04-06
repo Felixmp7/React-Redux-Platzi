e5756e1906c5a9262ad796b2324ea06ca8b3c95d
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-06 11:09:13 -0400

    Transformando a constante la propiedad que voy a traer mediante props

--- platzi-video/src/player/components/Video.js
+++ platzi-video/src/player/components/Video.js
@@ -20,12 +20,16 @@ class Video extends Component {
     this.video = element
   }
   render() {
+    const {
+      handleLoadedMetadata,
+    } = this.props
     return(
       <div className="Video">
         <video
           ref={this.setRef}
           autoPlay={this.props.autoPlay}
           src={this.props.src}
+          onLoadedMetadata={handleLoadedMetadata}
         />
       </div>
     )
