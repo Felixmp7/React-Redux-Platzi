5450766c495f34b26d88a9fd074eaa76ba8b88af
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-08 12:19:11 -0400

    Implementando Volume component

--- platzi-video/src/player/components/Volume.js
+++ platzi-video/src/player/components/Volume.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import VolumeIcon from '../../icons/components/Volume'
+import './Volume.css'
 
-const Volume = (props) => {
+const Volume = props => {
   return (
-
+    <button className="Volume">
+      <VolumeIcon
+        color="white"
+        size={25}
+      />
+      <div className="Volume-range">
+        <input type="range"/>
+      </div>
+    </button>
   );
 }
 
