ca389cfc59b9ea67446510616b5add93ddb3baa1
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-08 12:24:50 -0400

    Añadiendole props al input e invocando evento onChange

--- platzi-video/src/player/components/Volume.js
+++ platzi-video/src/player/components/Volume.js
@@ -10,7 +10,13 @@ const Volume = props => {
         size={25}
       />
       <div className="Volume-range">
-        <input type="range"/>
+        <input
+          type="range"
+          min={0}
+          max={1}
+          step={.05}
+          onChange={props.handleVolumeChange}
+        />
       </div>
     </button>
   );
