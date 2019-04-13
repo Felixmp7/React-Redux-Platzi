27460f9dd944b80aadee0dab6de1fe8cb25a145c
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-13 13:01:20 -0400

    Creando el store

--- platzi-video/src/entries/redux.js
+++ platzi-video/src/entries/redux.js
@@ -1,1 +1,31 @@
-console.log('ola ke ase');
+import { createStore } from 'redux'
+// console.log('ola ke ase');
+
+const form = document.getElementById('form')
+form.addEventListener('submit',handleSubmit)
+
+function handleSubmit(event) {
+  event.preventDefault()
+
+  const data = new FormData(form) // FormData es una clase que podemos utilizar para
+                             //  guardar los datos, en éste caso los del formulario
+  const title = data.get('title')
+  console.log(title)
+}
+
+const initialState = [
+  {
+    'title': 'Despacito'
+  },
+  {
+    'title': 'One more time'
+  },
+  {
+    'title': 'Echame la culpa'
+  },
+]
+const store = createStore(
+  () => {},   // reducer,
+  initialState,   // Estado inicial de nuestra aplicacion,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()  // enhancer,
+)
