299efb528c7a32e28b6989825e3019c8c5faa081
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-13 14:29:30 -0400

    Re-renderizando elementos en la UI

--- platzi-video/src/entries/redux.js
+++ platzi-video/src/entries/redux.js
@@ -47,7 +47,7 @@ const reducer = function(state,action){
     case 'ADD_SONG':
       return [...state, action.payload]
     break;
-    
+
     default: return state
   }
 }
@@ -58,13 +58,24 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()  // enhancer,
 )
 
-const container = document.getElementById('playlist') //Referenciando div del html
-const playlist = store.getState() // Guardando datos del estore en una constante
-// console.log(store.getState()); // Obteniendo datos del store
+function render(){
+  const container = document.getElementById('playlist') //Referenciando div del html
+  const playlist = store.getState() // Guardando datos del estore en una constante
+  // console.log(store.getState()); // Obteniendo datos del store
+  container.innerHTML = '' // Con ésto logro resolver el duplicado de elementos del store
 
-playlist.forEach( item => { // Recorriendo el array de objetos
-    const template = document.createElement('p') // Creo un elemento <p> y lo guardo en template
-    template.textContent = item.title // Le añado contenido al tag <p> proporcionado por la data
-                                      // que captura el form = item.title
-    container.appendChild(template) // Le añado a container cada template que creo
-})
+  playlist.forEach( item => { // Recorriendo el array de objetos
+      const template = document.createElement('p') // Creo un elemento <p> y lo guardo en template
+      template.textContent = item.title // Le añado contenido al tag <p> proporcionado por la data
+                                        // que captura el form = item.title
+      container.appendChild(template) // Le añado a container cada template que creo
+  })
+}
+
+render()
+
+function handleChange(){
+  render()
+}
+
+store.subscribe(handleChange)
