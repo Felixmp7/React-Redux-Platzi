7e928e603caacd47f52185367eb625e69444f846
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-13 19:50:55 -0400

    Importando el createStore, creando el store y asignando data al initialState

--- platzi-video/src/entries/home.js
+++ platzi-video/src/entries/home.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import {render} from 'react-dom'
+import { createStore } from 'redux'
 import Home from '../pages/containers/home'
 import data from '../api.json'
+
+const initialState = {
+  data: {
+    ...data
+  }
+}
+const store = createStore(
+  (state) => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 const homeContainer = document.getElementById('home-container')
 render(<Home data={data}/>, homeContainer)
