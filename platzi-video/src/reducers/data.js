bb5a86e83d08056089eda2f974c94a43befa2398
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-13 21:07:50 -0400

    Guardando el nuevo array en results

--- platzi-video/src/reducers/data.js
+++ platzi-video/src/reducers/data.js
@@ -2,7 +2,7 @@ function data(state,action){
   switch (action.type) {
     case 'SEARCH_VIDEO': {
       const list = state.data.categories[2].playlist
-      list.filter((item)=>{
+      const results = list.filter((item)=>{
         return item.author.include(action.payload.query) // Acá estoy comparando
         // si el author incluye dentro de sus valores lo que viene por payload...
         // entonces es true y me va a devolver un nuevo arreglo
