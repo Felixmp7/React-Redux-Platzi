3a9321090f9bb80a9325cdd5fb131c4983b7f74a
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-13 21:32:22 -0400

    Renderizando los videos que coincidan con la busqueda

--- platzi-video/src/categories/components/Categories.js
+++ platzi-video/src/categories/components/Categories.js
@@ -2,11 +2,18 @@ import React from 'react'
 import Category from './Category'
 import './Categories.css'
 import SearchContainer from '../../widgets/containers/SearchContainer'
+import Media from '../../playlist/components/Media'
 
 const Categories = props => {
   return(
     <div className="Categories">
       <SearchContainer/>
+      {
+        props.search.map(item =>{
+          // console.log(item);
+          return <Media key={item.id} {...item} />
+        })
+      }
       {
         props.categories.map( item => {
           return (
