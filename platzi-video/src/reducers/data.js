91eb615a1364dc4d288a46cc00a0e1462f10faf1
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-13 21:07:34 -0400

    Implementando metodo de filtrado de busqueda

--- platzi-video/src/reducers/data.js
+++ platzi-video/src/reducers/data.js
@@ -1,7 +1,16 @@
 function data(state,action){
   switch (action.type) {
     case 'SEARCH_VIDEO': {
-      return state
+      const list = state.data.categories[2].playlist
+      list.filter((item)=>{
+        return item.author.include(action.payload.query) // Acá estoy comparando
+        // si el author incluye dentro de sus valores lo que viene por payload...
+        // entonces es true y me va a devolver un nuevo arreglo
+      })
+      return {
+        ...state,
+        search: []
+      }
     }
     default: return state
   }
