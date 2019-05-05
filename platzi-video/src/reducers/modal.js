61dcdcff6d09c64b7af146b077988a771fc74d19
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-05 15:25:51 -0400

    Creando reducer sencillo para el modal y un rootReducer para importar todos los reducers en un solo archivo

---
+++ platzi-video/src/reducers/modal.js
@@ -0,0 +1,12 @@
+function modal(state,action){
+  switch (action.type) {
+    case 'OPEN_MODAL':
+      return state
+    case 'CLOSE_MODAL':
+      return state
+    default:
+      return state
+  }
+}
+
+export default modal
