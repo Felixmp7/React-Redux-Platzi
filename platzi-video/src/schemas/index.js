90e7d6334fc5c2611565fc3940df5074b6570f20
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-05 13:40:31 -0400

    Creando primer schema

--- platzi-video/src/schemas/index.js
+++ platzi-video/src/schemas/index.js
@@ -1,5 +1,17 @@
 import api from '../api.json'
 import { normalize, schema } from 'normalizr'
 
-const media = new schema.Entity()
+// const media = new schema.Entity(key,definicion del esquema, opciones)
+const media = new schema.Entity('media',{}, {
+  idAttribute: 'id', //Por defecto el toma el key id por defecto
+  //  la propiedad idAttribute se utiliza por si nuestro key no se llama 'id'
+  processStrategy: (value,parent,key) => ({...value, category: parent.id})
+})
 const category = new schema.Entity()
+
+const categories =
+
+// const normalizeData = normalize(datos iniciales, datos normalizados)
+const normalizeData = normalize(api,categories)
+
+export default normalizeData
