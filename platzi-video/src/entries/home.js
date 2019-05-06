5bfd743f9a2edfa92b5e98150e38876b30f89039
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-06 19:27:27 -0400

    Construyendo primer middleware

--- platzi-video/src/entries/home.js
+++ platzi-video/src/entries/home.js
@@ -1,29 +1,27 @@
 import React from 'react'
 import {render} from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import Home from '../pages/containers/home'
-// import data from '../schemas'
 import reducer from '../reducers/index'
 import { Map as map } from 'immutable'
 
-// console.log(data);
-
-// const initialState = {
-//   data: {
-//     entities: data.entities,
-//     categories: data.result.categories,
-//     search: [],
-//   },
-//   modal: {
-//     visibility: false,
-//     mediaID: null,
-//   }
-// }
+// Construccion de un middleware
+function logger({dispatch,getState}){
+  return (next) => {
+    return (action) => {
+      console.log('vamos a enviar ésta acción', action);
+      const value = next(action)
+      console.log('Éste es mi nuevo estado', getState().toJS());
+      return value
+    }
+  }
+}
 const store = createStore(
   reducer,
   map(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(logger)
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 const homeContainer = document.getElementById('home-container')
