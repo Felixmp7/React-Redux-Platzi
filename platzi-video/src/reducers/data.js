2bb5e745253dbf0e00b8d3433bf61693a93d85d2
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-06 12:44:37 -0400

    Añadiendo comentarios Teóricos

--- platzi-video/src/reducers/data.js
+++ platzi-video/src/reducers/data.js
@@ -22,3 +22,19 @@ function data(state = initialState,action){
 }
 
 export default data
+
+/*
+Resumen de métodos básicos de immutableJS sobre un mapa (objeto mapeado).
+
+mapa.get('key'): Obtiene el valor de ‘key’ en el mapa.
+
+mapa.getIn(['key','sub-key']): Obtiene el valor de ‘sub-key’, que es un mapa
+hijo (de segundo nivel) asociado a ‘key’ en el mapa original.
+
+mapa.set('key','valor'): Retorna un nuevo mapa donde el valor de ‘key’ ha
+sido remplazado por ‘valor’.
+
+mapa.merge({key1:'valor1',key2:'valor2'}): Retorna un nuevo mapa que es el
+resultado de mezclar (consolidar) el mapa original y el mapa pasado por
+parametro como objeto.
+*/
