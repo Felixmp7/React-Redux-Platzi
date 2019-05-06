ce3fd299c33f28645bad9c11e600aade5e70f454
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-06 12:41:15 -0400

    Arreglando bug del case sensitive en el includes

--- platzi-video/src/pages/containers/home.js
+++ platzi-video/src/pages/containers/home.js
@@ -66,7 +66,7 @@ function mapStateToProps(state, props){
   if (search) {
     const mediaList = state.get('data').get('entities').get('media')
     searchResults = mediaList.filter(item => {
-      return item.get('author').toLowerCase().includes(search).toLowerCase()
+      return item.get('author').toLowerCase().includes(search.toLowerCase())
     }).toList()
   }
   return {
