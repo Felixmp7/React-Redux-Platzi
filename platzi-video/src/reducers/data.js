17c5607eaadc5e51ff677868ed49b5b4b63388d0
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-06 12:24:37 -0400

    Retornando en el reducer de data un nuevo estado inmutable, en el key search el valor del action.payload.query

--- platzi-video/src/reducers/data.js
+++ platzi-video/src/reducers/data.js
@@ -4,26 +4,32 @@ import { fromJS } from 'immutable'
 const initialState = fromJS({
   entities: schema.entities,
   categories: schema.result.categories,
-  search: [],
+  search: '',
 })
 
 function data(state = initialState,action){
   switch (action.type) {
     case 'SEARCH_VIDEO': {
-      let results = []
-      if (action.payload.query) {
-        const list = state.data.categories
-        list.map( categoria =>{
-              let tempResults = categoria.playlist.filter(item=>{
-                return item.author.toLowerCase().includes(action.payload.query.toLowerCase())
-              })
-              results = results.concat(tempResults)
-        })
-      }
-      return {
-        ...state,
-        search: results
-      }
+      // let results = []
+      // if (action.payload.query) {
+      //   const list = state.data.categories
+      //   list.map( categoria =>{
+      //         let tempResults = categoria.playlist.filter(item=>{
+      //           return item.author.toLowerCase().includes(action.payload.query.toLowerCase())
+      //         })
+      //         results = results.concat(tempResults)
+      //   })
+      // }
+      // return {
+      //   ...state,
+      //   search: results
+      // }
+      return state.set('search', action.payload.query)
+      /*
+        Al haber transformado el estado en un mapa, puedo hacer como que seteo el state
+        directamente state.set() y lo que va a hacer es sacar una copia automatica para crear
+        un nuevo estado inmutable
+      */
     }
     default: return state
   }
